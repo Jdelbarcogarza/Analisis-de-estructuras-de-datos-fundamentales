f3758b4afc92622a98862d19be070530cdf77fcd
Jorge Del Barco <jdelbarcogarza@gmail.com>
2021-10-11 18:58:45 -0500

    métodos visit y height temrinados

--- Act3.1/BST.h
+++ Act3.1/BST.h
@@ -6,6 +6,7 @@ class BST{
         
         Node *root;
 
+
     public:
 
         BST();
@@ -15,7 +16,9 @@ class BST{
         void remove(int data);
         void destroy(Node* node);
         void visit(int order);
-
+        int height();
+        void ancestors(int data);
+ 
 };
 
 
@@ -185,8 +188,122 @@ void BST::remove(int data){
 
 }
 
-void visit(int order){
+void preOrder(Node* root){
 
+    if (root != nullptr){
+
+        std::cout << root->getData() << " ";
+        preOrder(root->getLeft());
+        preOrder(root->getRight());
+    }
+}
+
+void inOrder(Node* root){
+
+    if( root != nullptr){
+
+        inOrder(root->getLeft());
+        std::cout << root->getData() << " ";
+        inOrder(root->getRight());
+    }
+
+}
+
+void postOrder(Node* root){
+
+    if (root != nullptr){
+
+        postOrder(root->getLeft());
+        postOrder(root->getRight());
+        std::cout << root->getData() << " ";
+    }
+}
+
+void byLevel(Node* root){
     
+    std::cout << "PENDIENTE";
+    /*
+    Node* father = root;
+
+    if(root != nullptr){
+
+    std::cout << root->getData() << " ";
+    std::cout << root->getLeft()->getData() << " ";
+    std::cout << root->getRight()->getData() <<  " ";
+    
+    }
+    byLevel(root->getLeft());
+    */
+}
+
+/**
+ * Desplegará cada uno de los datos almacenados en el BST dependiendo del parámetro en entrada:
+
+    Preorder
+    inOrder
+    postOrder
+    Level by level
+
+    Parámetros:
+    Un entero (1-4) indicando la opción del tipo de visita.
+*/
+void BST::visit(int order){
+
+    switch (order)
+    {
+    case 1:
+        
+        preOrder(root);
+        break;
+
+    case 2:
+
+        inOrder(root);
+        break;
+   
+    case 3:
+
+        postOrder(root);
+        break;
+
+    case 4:
+
+        byLevel(root);
+        break;
+
+    default:
+        break;
+    }
+
+}
+
+
+/* Función escondida que encuentra la altura de un arbol recursivamente.*/
+int bstHeight(Node* root){
+
+    if (root == nullptr){
+            return 0;
+        }
+
+    int leftHeight = bstHeight(root->getLeft());
+    int rightHeight = bstHeight(root->getRight());
+
+    // sumar 1 al lado que tenga más altura
+    return  leftHeight > rightHeight ? leftHeight + 1 : rightHeight + 1;
+
+}
+
+
+/* Función que devuelve la altura de un BST*/
+int BST::height(){
+
+    // devolvemos el valor de altura que encontramos con la función.
+    return bstHeight(root);
+    
+}
+
+/* Función que visita los ancestros de un elemento del árbol.*/
+void BST::ancestors(int data){
+
 
 }
