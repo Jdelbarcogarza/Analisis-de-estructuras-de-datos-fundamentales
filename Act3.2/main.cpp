0ba06fb04e695fbb4c8d856ccbdd651b3a156048
Jorge Del Barco <jdelbarcogarza@gmail.com>
2021-10-19 15:20:42 -0500

    main con pruebas realizadas

--- Act3.2/main.cpp
+++ Act3.2/main.cpp
@@ -3,16 +3,18 @@
 
 using namespace std;
 
-int main(void){
+int main(void)
+{
 
     priority_queue list;
 
-    int A[10] = {16,14,10,8,7,9,3,2,4,1};
-
-    for (int i = 0; i < 10; i++){
+    int A[10] = {16, 14, 10, 8, 7, 9, 3, 2, 4, 1};
+    int B[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    
+    for (int i = 0; i < 1; i++)
+    {
         list.push(A[i]);
     }
-
 
     /*
     Como manejamos un priority queue con 3 o menos elementos.
@@ -20,11 +22,17 @@ int main(void){
     MAX heapify se hace con un arreglo ya establecido. No cuando se le van insertando valores-
     recursivamente hace que los valores vayan flotando hacia arriba o hacia abajo.
     */
-
-   for (int i = 0; i < 10; i++){
-   list.pop();
-   }
-
     list.print();
 
+    
+
+    for (int i = 0; i < 3; i++)
+    {
+        if (i == 1) { list.pop();}
+        cout << "El top es: "<< list.top() << "\n";
+        cout << list.empty();
+    }
+   
+    
+    list.print();
 }
