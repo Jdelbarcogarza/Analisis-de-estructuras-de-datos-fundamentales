44ffa2cfd84946b9199c890105559d2f621cfa7c
Jorge Del Barco <jdelbarcogarza@gmail.com>
2021-11-09 00:43:37 -0600

    función loadGraph funcional: pendiente aprobación del profe

--- Act4.1/appGrafo.cpp
+++ Act4.1/appGrafo.cpp
@@ -7,25 +7,37 @@
 using namespace std;
 
 
-void loadGraph(LinkedList<int> adjList[], int nodeAmount, int edgeAmount);
+void loadGraph(LinkedList<int> adjList[], int nodeAmount, int edgeAmount, string filename);
+void printAdjList(LinkedList<int> adjList[], int nodeAmount);
 
 int main(void){
 
-    fstream in("in1.txt");
+    string filename = "in1.txt";
+
+    fstream in(filename);
 
     int nodeAmount, edgeAmount, rootNode; // characteristic variables
     
     // get n, m and s
     in >> nodeAmount >> edgeAmount >> rootNode;
 
-    // array that will contain the linked lists on each index.
-    LinkedList<int> *adjList = new LinkedList<int>[nodeAmount];
+    /* SOLO NECESITO 4 NODOS PORQUE LAS COMBINACIONES YA SE DIERON EN TODOS LOS NODOS ANTERIORES
+    COMO QUIERA CHECARLO CON EL PROFE.*/
+    nodeAmount = nodeAmount + 1;
 
-    loadGraph(adjList, nodeAmount, edgeAmount);
+    // array that will contain the linked lists on each index.
+    /* Since we want to start adding numbers from the indexes that we have, (in this case we don't have
+    zero) we just print */
+    LinkedList<int> *adjList = new LinkedList<int>[nodeAmount];
 
     int pointA, pointB, edgeWeight;
 
+    
 
+    loadGraph(adjList, nodeAmount, edgeAmount, filename);
+    
+
+    printAdjList(adjList, nodeAmount);
 
     // delete used memory
     delete[] adjList;
@@ -33,15 +45,46 @@ int main(void){
 
 }
 
-void loadGraph(LinkedList<int> adjList[], int nodeAmount, int edgeAmount){
+void loadGraph(LinkedList<int> adjList[], int nodeAmount, int edgeAmount, string filename){
 
-    adjList[0].addLast(34);
-    adjList[0].addLast(3);
-    adjList[0].addLast(4);
-    adjList[0].addLast(56);
+    fstream in(filename);
 
-    for (int i = 0; i < 1; i++){
-        adjList->print();
+
+    int pointA, pointB, edgeWeight;
+
+    // we read input to ignore first line.
+    in >> pointA >> pointB >> edgeWeight;
+
+    string s;
+    // here we start from one bc we want to ignore the 0 index that is empty, but this will not
+    // always be the case.
+    for (int i = 1; i < edgeAmount; i++){
+        
+        in >> pointA >> pointB >> edgeWeight;
+        
+        cout << pointA << " " << pointB << " " << edgeWeight << endl;
+        adjList[pointA].addFirst(pointB, edgeWeight);
+
+        // PREGUNTALE AL PROFE SI ESTO ESTÁ BIEN.
+        /* EN TEORÍA ES UN GRAFO NO DIRIGIDO, POR LO QUE LOS NODOS PUEDEN INTERCONECTARSE POR ESO
+        LA LÍNEA  DEBAJO ES VÁLIDA*/
+        adjList[pointB].addFirst(pointA, edgeWeight);
+        
+    }
+
+}
+
+void printAdjList(LinkedList<int> adjList[], int nodeAmount){
+
+    // ¿Por qué en adjList[0].print() no hay elementos?
+    for (int i = 0; i < nodeAmount; i++){
+        
+        // si la lista está vacía no imprimas 
+        if (adjList[i].getSize() == 0){
+            continue;
+        }
+        cout << "[" << i << "] --> ";
+        adjList[i].print();
     }
 
 }
