6f7030279371e70721c40e6de6ab234f997fea75
Jorge Del Barco <jdelbarcogarza@gmail.com>
2021-11-18 16:49:35 -0600

    termine actividad 4.1

--- Act4.1/Node.h
+++ Act4.1/Node.h
@@ -1,67 +1,39 @@
-
-template <class T>
+template<class T>
 class Node{
 
-
     private:
-        T data;
-        T weight;
-        Node<T>* next;
 
-    public: 
-        Node(T data, T weight); // constructor
-        Node(T data, T weight, Node<T>* next); // constructor
-        T getData();
-        Node<T>* getNext();
-        T getWeight();
-        void setData(T data);
-        void setNext(Node<T>* next);
-        void setWeight(T weight);
+        T origin;
+        T destination;
+        T weight;
+
+    public:
+
+        Node(){
+            this->origin = 0;
+            this->destination = 0;
+            this->weight = 0;
+        }
+
+        Node(T origin, T destination, T weight){
+            this->origin = origin;
+            this->destination = destination;
+            this->weight = weight;
+
+        }
+
+        T getOrigin(){
+            return origin;
+        }
+
+        T getDestination(){
+            return destination;
+        }
+
+        T getWeight(){
+            return weight;
+        }
+
+        // podemos agregar los setters pero por el momento creo que no son necesarios.
 
 };
-
-
-template <class T> // Declaración de tipo 'T'.
-Node<T>::Node(T data, T weight){
-    this->data = data;
-    this->weight = weight;
-    this->next = nullptr; // NULL definido en librería std para apuntadores.
-
-}
-
-template <class T>
-Node<T>::Node(T data, T weight, Node<T>* next){
-    this->data = data;
-    this->weight = weight;
-    this->next = next;
-}
-
-template <class T>
-T Node<T>::getData(){
-    return data;
-}
-
-template <class T>
-T Node<T>::getWeight(){
-    return weight;
-}
-
-template <class T>
-Node<T>* Node<T>::getNext(){
-    return next;
-}
-
-template <class T>
-void Node<T>::setData(T data){
-    this->data = data;
-}
-
-template <class T>
-void Node<T>::setNext(Node<T>* next){
-    this->next = next;
-}
-
-template <class T>
-void Node<T>::setWeight(T weight){
-    this->weight = weight;
-}
--- Act4.1/appGrafo.cpp
+++ Act4.1/appGrafo.cpp
@@ -1,54 +1,178 @@
 #include <iostream>
-#include <list>
 #include <vector>
+#include <queue>
 #include <fstream>
-#include "LinkedList.h"
-
+#include "Node.h"
 using namespace std;
 
+int timeCounter;
+vector<bool> dfsVisited;
 
-void loadGraph(LinkedList<int> adjList[], int nodeAmount, int edgeAmount, string filename);
-void printAdjList(LinkedList<int> adjList[], int nodeAmount);
+void BFS(vector<vector<Node<int>>> &adjList, int rootNode);
+void DFS(vector<vector<Node<int>>> &adjList, int rootNode);
+void DFSVisit(vector<vector<Node<int>>> &adjList, vector<Node<int>>&adjNodes);
+void loadGraph(vector<vector<Node<int>>> &adjList, int nodeAmount, int edgeAmount, string filename);
+void printList(vector<vector<Node<int>>> &adjList, int nodeAmount);
 
 int main(void){
 
-    string filename = "in1.txt";
-
-    fstream in(filename);
-
-    int nodeAmount, edgeAmount, rootNode; // characteristic variables
+    string filename = "in2.txt";
     
-    // get n, m and s
+    fstream in(filename);
+    int nodeAmount, edgeAmount, rootNode; // characteristic variables of graph
+    
     in >> nodeAmount >> edgeAmount >> rootNode;
 
-    /* SOLO NECESITO 4 NODOS PORQUE LAS COMBINACIONES YA SE DIERON EN TODOS LOS NODOS ANTERIORES
-    COMO QUIERA CHECARLO CON EL PROFE.*/
-    nodeAmount = nodeAmount + 1;
+    in.close();
 
-    // array that will contain the linked lists on each index.
-    /* Since we want to start adding numbers from the indexes that we have, (in this case we don't have
-    zero) we just print */
-    LinkedList<int> *adjList = new LinkedList<int>[nodeAmount];
-
-    int pointA, pointB, edgeWeight;
-
-    
+    // crear lista de adyacencia con nodeAmpount espacios.
+    vector<vector<Node<int>>> adjList(nodeAmount + 1); // lo hago +1 por si llega a existir un nodo 0.
 
     loadGraph(adjList, nodeAmount, edgeAmount, filename);
     
+    printList(adjList, nodeAmount);
 
-    printAdjList(adjList, nodeAmount);
+    //cout <<"here: " << adjList[0].size();
 
-    // delete used memory
-    delete[] adjList;
-    return 0;
+    
+    cout << "BFS: ";
+    BFS(adjList, rootNode);
+    
+    cout << "DFS: ";
+    
+    // set all nodes to not visited
+    for (unsigned int i = 0; i < adjList.size(); i++){
+        dfsVisited.push_back(false);   
+    }
 
+    DFS(adjList, rootNode); 
+    
+}
+// Para DFS
+// in1: 1 2 4 5 3
+// in2: 1 3 5 6 7 8 9 10 4 2
+
+void DFSVisit(vector<vector<Node<int>>> &adjList, vector<Node<int>>&adjNodes){
+
+    timeCounter = timeCounter + 1;
+
+    // itera sobre la lista que pasaste como parámetro
+    for (unsigned int i = 0; i < adjNodes.size(); i++){
+    //cout << "who" << adjNodes[i].getDestination() << endl;
+    
+        // si el primer nodo de esa lista de adyacencia no ha sido visitado
+        if (dfsVisited[adjNodes[i].getDestination()] == false){
+            // imprime el nodo
+            cout << adjNodes[i].getDestination() << " ";
+
+            // marcalo como visitado
+            dfsVisited[adjNodes[i].getDestination()] = true;
+
+            // ahora ese nodo lo debes pasar para buscar el siguiente conectado a ese.
+            DFSVisit(adjList, adjList[adjNodes[i].getDestination()]);
+        } 
+    }
+
+    return;
+    // pasas la lista de vecinos del nodo siguiente, no los nodos vecinos del actual nodo
+    // esto ni se está corriendo creo
+    DFSVisit(adjList, adjList[adjNodes[0].getDestination()]);
+    
+    timeCounter = timeCounter + 1;
 }
 
-void loadGraph(LinkedList<int> adjList[], int nodeAmount, int edgeAmount, string filename){
+void DFS(vector<vector<Node<int>>> &adjList, int rootNode){
+
+    timeCounter = 0;
+    
+    if (dfsVisited[adjList[rootNode][0].getOrigin()] == true){
+      return;
+    }
+
+    // imprime el primer nodo que visitaste
+    cout << adjList[rootNode][0].getOrigin() << " ";
+
+    for (unsigned int currentNode = rootNode; currentNode < adjList.size()-1; currentNode++){
+      //cout << currentNode << endl;
+            
+            // si no hemos visitado el nodo. Lo visitamos con DFS-Visit
+            // adjList[currentNode][0].getOrigin() -> 1
+        if (dfsVisited[adjList[currentNode][0].getOrigin()] == false){
+            
+            // ponemos el nodo fuente como ya visitado
+            dfsVisited[adjList[rootNode][0].getOrigin()] = true;
+
+            // pasamos la lista entera y la lista de adjacencia del currentNode 
+            DFSVisit(adjList, adjList[currentNode]);
+        }       
+    }
+      // ponemos el nodo fuente como ya visitado
+        dfsVisited[adjList[rootNode][0].getOrigin()] = true;
+    
+    // edge case para casos donde no hay un nodo conectado
+    for (unsigned int i = 0; i < dfsVisited.size(); i++){
+        
+        // si no haz visitado ese nodo. Hazle dfs
+        if (dfsVisited[i] == false && adjList[i].size() != 0){
+            // hazle DFS a ese nodo
+            DFS(adjList, i);
+        }
+    }
+}
+
+void BFS(vector<vector<Node<int>>> &adjList, int rootNode){
+
+    queue<int> gray; // processed nodes
+    vector<bool> visited;
+    int currentNode;
+    
+    // set all nodes to not visited
+    for (unsigned int i = 0; i < adjList.size(); i++){
+        visited.push_back(false);    
+    }
+
+    // aqui se hace un push del nodo en el que estamos
+    gray.push(adjList[rootNode][0].getOrigin());
+    
+    // el nodo inicial lo marcas como visitado
+    visited[rootNode] = true;
+
+    // OJO: EL LOOP PODRÍA SER SIZE() - 1 PORQUE NO ESTAMOS CONTANDO EL CERO PERO SÍ ESTÁ AHÍ. CUIDADO
+
+    while(!gray.empty()){
+
+        currentNode = gray.front();
+        gray.pop();
+
+        // OUTPUT
+        cout << currentNode;
+
+        for (unsigned int adjNode = 0; adjNode < adjList[currentNode].size(); adjNode++){
+
+            //cout << adjList[currentNode][adjNode].getDestination() << " ";
+            
+            // si ya está visitado ese nodo, pasa al siguiente.
+            if (visited[adjList[currentNode][adjNode].getDestination()] == true){
+                continue;
+                
+            } else {
+                // si no lo haz visitado. Marcalo como visitado
+                visited[adjList[currentNode][adjNode].getDestination()] = true;
+            }
+
+            // add nodes attached to currentNode
+            gray.push(adjList[currentNode][adjNode].getDestination());
+
+        }
+            cout << " ";   
+    }
+    cout << endl;
+}
+
+
+void loadGraph(vector<vector<Node<int>>> &adjList, int nodeAmount, int edgeAmount, string filename){
 
     fstream in(filename);
-
 
     int pointA, pointB, edgeWeight;
 
@@ -58,33 +182,35 @@ void loadGraph(LinkedList<int> adjList[], int nodeAmount, int edgeAmount, string
     string s;
     // here we start from one bc we want to ignore the 0 index that is empty, but this will not
     // always be the case.
-    for (int i = 1; i < edgeAmount; i++){
+    for (int i = 0; i < edgeAmount; i++){
         
         in >> pointA >> pointB >> edgeWeight;
-        
-        cout << pointA << " " << pointB << " " << edgeWeight << endl;
-        adjList[pointA].addFirst(pointB, edgeWeight);
+
+        adjList[pointA].push_back(Node<int>(pointA, pointB, edgeWeight));
 
         // PREGUNTALE AL PROFE SI ESTO ESTÁ BIEN.
         /* EN TEORÍA ES UN GRAFO NO DIRIGIDO, POR LO QUE LOS NODOS PUEDEN INTERCONECTARSE POR ESO
         LA LÍNEA  DEBAJO ES VÁLIDA*/
-        adjList[pointB].addFirst(pointA, edgeWeight);
-        
+        //adjList[pointB].push_back(Node<int>(pointB, pointA, edgeWeight));
     }
-
 }
 
-void printAdjList(LinkedList<int> adjList[], int nodeAmount){
+void printList(vector<vector<Node<int>>> &adjList, int nodeAmount){
 
-    // ¿Por qué en adjList[0].print() no hay elementos?
-    for (int i = 0; i < nodeAmount; i++){
-        
-        // si la lista está vacía no imprimas 
-        if (adjList[i].getSize() == 0){
+    for (unsigned int i = 0; i < adjList.size(); i++){
+
+        // don't print non-existing nodes.
+        if (adjList[i].size() == 0){
             continue;
         }
-        cout << "[" << i << "] --> ";
-        adjList[i].print();
+
+        cout << "[" << i << "]" << "--> ";
+        for (unsigned int j = 0; j < adjList[i].size(); j++){
+            cout << adjList[i][j].getDestination() << "|" << adjList[i][j].getWeight() << "--> ";
+        }
+
+        cout << "END" << endl;
+
     }
 
 }
